266a4d3a73f0dd8d3386cdaa696b006435e07240
lawrencedulian <lawrenceedulian@gmail.com>
2023-01-11 16:06:12 +0100

    added allert message

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,17 +1,28 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
-        <h2>My Projects</h2>
-
-        <div class="row">
+    <div class="container mt-4">
+        <h2 class="text-center">My Projects</h2>
+        <div class="text-end">
+            <a class="btn btn-success" href="{{ route('admin.projects.create') }}">
+                <i class="fa-solid fa-file-circle-plus"></i>
+                New
+            </a>
+        </div>
+        <div class="row justify-content-center">
             <div class="col-8">
+                @if (session('message'))
+                    <div class="alert alert-success">
+                        {{ session('message') }}
+                    </div>
+                @endif
+
                 <table class="table">
                     <thead>
                         <tr>
-                            <th scope="col">Titolo</th>
-                            <th scope="col">Data Creazione: </th>
-                            <th scope="col">Azioni</th>
+                            <th scope="col">Titolo:</th>
+                            <th scope="col">Data Creazione:</th>
+                            <th scope="col">Azioni:</th>
                         </tr>
                     </thead>
                     <tbody>
